540ee58f90c8672602dfa571de526d057e180f11
ny8nine <nhamilton1@gmail.com>
2016-11-27 13:02:03 -0500

    updated the projects section to add descriptions, and udpated js to display a random color on page load

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
-//document.getElementById("paintbrush").onclick = 'alert("clicked")';
+var paintbrush = document.getElementById("paintbrush");
 var highlight = ["#67D5B5", "#EE7785", "#C89EC4", "#84B1ED"];
+
+paintbrush.addEventListener('click', changeColor)
+
 
 //pick random number between 0 and max
 function picker(max) {
@@ -7,11 +10,10 @@ function picker(max) {
 }
 
 
-
-function myFunction() {
+function changeColor() {
 	var i = picker(highlight.length);
-	var color = highlight[i]; 
-	
+	var color = highlight[i];
+
   var text = document.getElementsByClassName("color");
   for (var i=0; i<text.length; i++){
   	text[i].style.color= color;
@@ -20,9 +22,7 @@ function myFunction() {
   for(var i=0; i<link.length; i++){
   	link[i].style.color=color;
   }
-
-  var image = document.getElementsByClassName("image");
-  for(var i=0; i<image.length; i++){
-  	image[i].style.fill=color;
-  }
 }
+
+
+window.onlaod = changeColor();
